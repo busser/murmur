aa5325d422f5aad078f8ab6e0ec31f7ca96ee37e
Arthur Busser <arthur.busser@gopigment.com>
2025-06-07 09:19:48 +0200

    fix Azure e2e test setup

--- terraform/layers/azure-keyvault/_providers.tf
+++ terraform/layers/azure-keyvault/_providers.tf
@@ -10,8 +10,7 @@ provider "azurerm" {
   subscription_id = "8ab3da27-5e1b-494f-abc6-726fb04729b3"
 
   # We don't need to register any resource providers.
-  # For more information: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs#skip_provider_registration
-  skip_provider_registration = false
+  resource_provider_registrations = "none"
 }
 
 # The "azuread" provider enables us to provision resources in Azure Active
